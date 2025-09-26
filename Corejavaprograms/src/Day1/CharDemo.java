9d1079e3033a1024683c632185748f80b09308e7
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-09-26 13:55:20 +0530

    commit

---
+++ Corejavaprograms/src/Day1/CharDemo.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'a';
+		System.out.println(ch);
+
+		// assigning number to char
+		char ch1 = 65;
+		System.out.println(ch1);
+
+		// assigning unicode to char
+		char var1 = '\u00A7';
+		System.out.println(var1);
+
+	}
+
+}
