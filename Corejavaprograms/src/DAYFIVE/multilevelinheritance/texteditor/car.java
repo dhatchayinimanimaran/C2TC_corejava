e10fb4a8148682a6f72dcbf5e54d05f191872936
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-02 17:52:13 +0530

    commit

---
+++ Corejavaprograms/src/DAYFIVE/multilevelinheritance/texteditor/car.java
@@ -0,0 +1,15 @@
+package DAYFIVE.multilevelinheritance.texteditor;
+
+public class car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public Car() {
+			System.out.println("Class Car");
+		}
+
+		public void vehicleType() {
+			System.out.println("Vehicle Type: Car");
+	}
+
+}
