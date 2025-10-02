bcff1257634b19b84dfee69f51755935e2a7e7cc
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-02 14:14:07 +0530

    commit

---
+++ Corejavaprograms/src/Day5/association/hasa/Exceutor.java
@@ -0,0 +1,12 @@
+package Day5.association.hasa;
+
+public class Exceutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Address address = new Address("405 Laxmi Enclave, Pandit Colony", "Nashik", "Maharashtra", "422003");
+		Person person = new Person("Aniket Pawar", address);
+		person.displayInfo();
+	}
+
+}
