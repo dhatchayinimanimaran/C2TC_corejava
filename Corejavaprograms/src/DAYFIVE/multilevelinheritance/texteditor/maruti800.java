1883813a112fc4593071111204003da4fc4f27e3
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-02 17:55:09 +0530

    commit

---
+++ Corejavaprograms/src/DAYFIVE/multilevelinheritance/texteditor/maruti800.java
@@ -0,0 +1,16 @@
+package DAYFIVE.multilevelinheritance.texteditor;
+
+public class maruti800 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public Maruti800() {
+			System.out.println("Maruti Model: 800");
+		}
+
+		public void speed() {
+			System.out.println("Max: 80Kmph");
+		
+	}
+
+}
