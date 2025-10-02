5c07efa7db371639aebc19c910f64ea21678265c
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-02 17:26:17 +0530

    commit

---
+++ Corejavaprograms/src/DAYFIVE/ASSOCIATION/hasa/Executor.java
@@ -0,0 +1,13 @@
+package DAYFIVE.ASSOCIATION.hasa;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			Address address = new Address("405 Laxmi Enclave, Pandit Colony", "Nashik", "Maharashtra", "422003");
+			Person person = new Person("Aniket Pawar", address);
+			person.displayInfo();
+	}
+
+}
