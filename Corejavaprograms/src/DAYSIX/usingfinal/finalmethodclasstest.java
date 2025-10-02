ff4c57e333e5d2ed4545ee0c133effb3b245dc24
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-02 18:16:42 +0530

    commit

---
+++ Corejavaprograms/src/DAYSIX/usingfinal/finalmethodclasstest.java
@@ -0,0 +1,20 @@
+package DAYSIX.usingfinal;
+
+public class finalmethodclasstest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Program to demonstrate final method 
+		package com.tnsif.daysix.usingfinal;
+
+		public class FinalMethodClassTest extends FinalMethodClass  {
+			  // Compile time error because we cannot override the final method 
+				/*
+				 * @Override void show() {
+				 * 
+				 * System.out.println("This is the final method of FinalMethodEx class"); }
+				 */
+
+	}
+
+}
