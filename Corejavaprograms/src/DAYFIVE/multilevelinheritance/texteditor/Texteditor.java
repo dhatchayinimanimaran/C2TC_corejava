69ad6a565cdcdbc9f0dc60589c233e7916102073
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-02 17:45:47 +0530

    commit

---
+++ Corejavaprograms/src/DAYFIVE/multilevelinheritance/texteditor/Texteditor.java
@@ -0,0 +1,28 @@
+package DAYFIVE.multilevelinheritance.texteditor;
+
+public class Texteditor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		protected String content;
+
+	    public Notepad() {
+	        this.content = "";
+	    }
+
+	    public void write(String text) {
+	        content += text; //content = content+text
+	    }
+
+	    public void displayContent() {
+	        System.out.println("Notepad Content: " + content);
+	    }
+	    
+	    
+	    /*
+	     * returntype funcName()
+	     * {
+	     * 		//statement
+	}
+
+}
