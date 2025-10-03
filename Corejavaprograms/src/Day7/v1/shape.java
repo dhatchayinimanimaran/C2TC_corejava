6aaabb26b6391aba03c081de56303e7af6a4751f
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-03 20:39:32 +0530

    commit

---
+++ Corejavaprograms/src/Day7/v1/shape.java
@@ -0,0 +1,17 @@
+package Day7.v1;
+
+public class shape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Member function
+		public void draw() {
+			System.out.println("Drawing a generic shape");
+		}
+
+		public void erase() {
+			System.out.println("Erasing a generic shape");
+
+	}
+
+}
