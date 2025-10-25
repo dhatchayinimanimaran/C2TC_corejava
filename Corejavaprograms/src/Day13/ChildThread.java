baa067a2f10d5f53e49b32c6e9e39a8ab73fb887
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:19:58 +0530

    commit

---
+++ Corejavaprograms/src/Day13/ChildThread.java
@@ -0,0 +1,29 @@
+package Day13;
+
+public class ChildThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int n;
+		private String msg;
+
+		// Constructor
+		public ChildThread(int n, String msg) {
+			this.n = n;
+			this.msg = msg;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 1; i <= n; i++) {
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					System.err.println("Thread interrupted: " + e.getMessage());
+				}
+				System.out.println(msg + i + " " + Thread.currentThread().getName());
+			}
+
+	}
+
+}
