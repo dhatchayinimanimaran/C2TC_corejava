cc1a7acc7b5f3b1ac6c0eb9b28612df7215d3db3
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:56:50 +0530

    commit

---
+++ Corejavaprograms/src/Day11/v1/Demo.java
@@ -0,0 +1,12 @@
+package Day11.v1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the program continues....");
+		int data=100/0;
+
+	}
+
+}
