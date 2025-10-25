20f05ce31f6ac4095da44407c93a1b13cba9806f
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:16:50 +0530

    commit

---
+++ Corejavaprograms/src/Day12/student.java
@@ -0,0 +1,61 @@
+package Day12;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int rollNo;
+		private String name;
+		private int[] marks;
+		private int noOfSubjects;
+		private float per;
+		
+		public int getRollNo() {
+			return rollNo;
+		}
+
+		public void setRollNo(int rollNo) {
+			this.rollNo = rollNo;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int[] getMarks() {
+			return marks;
+		}
+
+		public int getNoOfSubjects() {
+			return noOfSubjects;
+		}
+
+		public void setNoOfSubjects(int noOfSubjects) {
+			this.noOfSubjects = noOfSubjects;
+		}
+
+		
+		public void setMarks(int[] marks) {
+			this.marks = marks;
+		}
+
+		public void setPer(float per) {
+			this.per = per;
+		}
+
+		public float getPer()
+		{
+			return this.per;
+		}
+		
+		@Override
+		public String toString() {
+			return "Student [rollNo=" + rollNo + ", name=" + name + ", marks=" + Arrays.toString(marks) + ", noOfSubjects="
+					+ noOfSubjects + "Percentage : "+per+" ]";
+	}
+
+}
