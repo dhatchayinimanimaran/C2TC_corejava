d6493516f475bc2738c455d1c4477eb9008bf791
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 08:55:13 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/funtionalinterfaces/greetclass.java
@@ -0,0 +1,12 @@
+package Day8.interfaces.extendinginterfaces.funtionalinterfaces;
+
+public class greetclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@Override
+		public String greet() {		
+			return "Welcome to the world of Java";
+	}
+
+}
