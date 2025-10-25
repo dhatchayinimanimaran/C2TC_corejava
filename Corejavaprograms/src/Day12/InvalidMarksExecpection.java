f1a2252388cd3ba42c4f293cdbe70420501ab203
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:14:47 +0530

    commit

---
+++ Corejavaprograms/src/Day12/InvalidMarksExecpection.java
@@ -0,0 +1,16 @@
+package Day12;
+
+public class InvalidMarksExecpection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public InvalidMarksException() {
+			super();
+		}
+
+		public InvalidMarksException(String message) {
+			super(message);
+
+	}
+
+}
