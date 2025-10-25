50c7186919037d454897a367c15a3b4e9a2067f5
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:28:00 +0530

    commit

---
+++ Corejavaprograms/src/Day13/usingRunnable.java
@@ -0,0 +1,31 @@
+package Day13;
+
+public class usingRunnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread;
+		int high, low;
+		String msg;
+
+		public UsingRunnable(int low, int high, String msg) {
+			this.high = high;
+			this.low = low;
+			this.msg = msg;
+			thread = new Thread(this, "Child Thread");
+			thread.start();
+		}
+
+		@Override
+		public void run() {
+			for (int i = low; i <= high; i++) {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					System.out.println("Error " + e.getMessage());
+				}
+				System.out.println(msg + i);
+			}
+	}
+
+}
