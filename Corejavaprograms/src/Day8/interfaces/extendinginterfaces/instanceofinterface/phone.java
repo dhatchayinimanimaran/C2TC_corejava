5ff048cd9c3338892eccf1edeac741512b50e573
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:10:48 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/instanceofinterface/phone.java
@@ -0,0 +1,12 @@
+package Day8.interfaces.extendinginterfaces.instanceofinterface;
+
+public class phone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		void call();
+
+		void sms();
+	}
+
+}
