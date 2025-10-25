2611a1b2c1a9e437d1f21532310fd3f36fa01fe2
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 08:43:34 +0530

    commit

---
+++ Corejavaprograms/src/Day8/abstraction/Rectangle.java
@@ -0,0 +1,23 @@
+package Day8.abstraction;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public Rectangle() {
+			this.width = 5.0f;
+			this.height = 2.0f;
+		}
+
+		public Rectangle(float width, float height) {
+			this.width = width;
+			this.height = height;
+		}
+
+		@Override
+		void calArea() {
+			area=width*height;	
+
+	}
+
+}
