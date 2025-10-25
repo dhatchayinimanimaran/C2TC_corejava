c7d2c3d096184d54a372c7db5a48872840183081
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:03:02 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/implementinginterfaces/Bank.java
@@ -0,0 +1,16 @@
+package Day8.interfaces.extendinginterfaces.implementinginterfaces;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double MIN_BALANCE = 1000;
+		 double DEPOSIT_LIMIT = 25000;
+		 double INTEREST_RATE = 7.8;
+
+		 void deposit(Account account, double amount);
+		 void withdraw(Account account, double amount);
+
+	}
+
+}
