be9ef4058b59dedf6764ccf3aec9b44a55d060a9
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 08:51:42 +0530

    commit

--- Corejavaprograms/src/Day8/interfaces/extendinginterfaces/extendinginterfaceDemo.java
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/extendinginterfaceDemo.java
@@ -4,6 +4,13 @@ public class extendinginterfaceDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		public void print() { 
+			System.out.println("print method");
+		}
+
+		// Override InterfaceOne method
+		public void show() {
+			System.out.println("show Method");
 
 	}
 
---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/interfaceone.java
@@ -0,0 +1,11 @@
+package Day8.interfaces.extendinginterfaces;
+
+public class interfaceone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		void print();  
+
+	}
+
+}
