ceb23783be1a84514cd3c7fa27bc30ab7d2a4aae
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:45:18 +0530

    commit

---
+++ Corejavaprograms/src/Day10/student.java
@@ -0,0 +1,31 @@
+package Day10;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int rollNo;
+		private String name;
+
+		Student(int rollNo, String name) {
+			this.rollNo = rollNo;
+			this.name = name;
+		}
+
+		public int getRollNo() {
+			return rollNo;
+		}
+
+		public void setRollNo(int rollNo) {
+			this.rollNo = rollNo;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+	}
+
+}
