748b7ae383bb10959459c6822db66215c0f3c10e
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:19:00 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/nestedinterfaces/Myinterface.java
@@ -0,0 +1,14 @@
+package Day8.interfaces.extendinginterfaces.nestedinterfaces;
+
+public class Myinterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print(); 
+
+	}
+
+}
