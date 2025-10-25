688c8d1b7200af693e13e6dcd82d3196193e1ec3
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:41:19 +0530

    commit

---
+++ Corejavaprograms/src/Day10/JaggedArrayDemo.java
@@ -0,0 +1,16 @@
+package Day10;
+
+public class JaggedArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d[][] = { { 10, 20, 30 }, { 40 }, { 12, 21 }, { 11 } };
+
+		System.out.println("Total Rows in Array d : " + d.length);
+
+		MLArray.printArray(d);
+
+
+	}
+
+}
