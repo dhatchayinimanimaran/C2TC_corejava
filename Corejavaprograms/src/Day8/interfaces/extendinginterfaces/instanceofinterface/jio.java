1521a84b0ceca7e565c58f112aa67d55de336c4f
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:09:48 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/instanceofinterface/jio.java
@@ -0,0 +1,19 @@
+package Day8.interfaces.extendinginterfaces.instanceofinterface;
+
+public class jio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@Override
+		public void call() {
+			System.out.println("Calling using Jio");
+		}
+
+		@Override
+		public void sms() {
+			System.out.println("Messaging using Jio");
+
+
+	}
+
+}
