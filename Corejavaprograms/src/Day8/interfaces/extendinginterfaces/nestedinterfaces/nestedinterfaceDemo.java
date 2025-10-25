764e9b213b72fde6eda724685feaac71ac96d255
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:21:23 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/nestedinterfaces/nestedinterfaceDemo.java
@@ -0,0 +1,15 @@
+package Day8.interfaces.extendinginterfaces.nestedinterfaces;
+
+public class nestedinterfaceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				NestedInterfaceClass obj = new NestedInterfaceClass();
+				obj.print();
+				System.out.println(NestedInterfaceClass.id);
+
+
+	}
+
+}
