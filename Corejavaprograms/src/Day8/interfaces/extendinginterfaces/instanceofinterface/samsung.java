c6ea2da38835f2a4201ce4671e60b78f06e39c10
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 09:13:09 +0530

    commit

---
+++ Corejavaprograms/src/Day8/interfaces/extendinginterfaces/instanceofinterface/samsung.java
@@ -0,0 +1,19 @@
+package Day8.interfaces.extendinginterfaces.instanceofinterface;
+
+public class samsung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@Override
+		public void call() {
+			System.out.println("Calling using Samsung");
+		}
+
+		@Override
+		public void sms() {
+			System.out.println("Messaging using Samsung");
+
+
+	}
+
+}
