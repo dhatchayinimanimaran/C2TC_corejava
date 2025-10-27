990c24900747f0754f10fa87311e7e72a5cd94b2
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-27 19:54:33 +0530

    commit

---
+++ Corejavaprograms/src/Day19/v1/cube.java
@@ -0,0 +1,13 @@
+package Day19.v1;
+
+public class cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@FunctionalInterface
+		interface Cube {
+			int calculate(int a); // only one abstract method
+			//int sqr(int n);
+	}
+
+}
