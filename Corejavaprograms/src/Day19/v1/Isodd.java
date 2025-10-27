7125098211a003517418c890b2916c43a067dc46
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-27 19:55:32 +0530

    commit

---
+++ Corejavaprograms/src/Day19/v1/Isodd.java
@@ -0,0 +1,13 @@
+package Day19.v1;
+
+public class Isodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@FunctionalInterface
+		interface Cube {
+			int calculate(int a); // only one abstract method
+			//int sqr(int n);
+	}
+
+}
