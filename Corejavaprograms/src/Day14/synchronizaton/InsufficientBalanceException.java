98e170412c3086bb9d29b4773eb2edb746651523
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-27 19:43:50 +0530

    commit

---
+++ Corejavaprograms/src/Day14/synchronizaton/InsufficientBalanceException.java
@@ -0,0 +1,15 @@
+package Day14.synchronizaton;
+
+public class InsufficientBalanceException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public InsufficientBalanceException() {
+			super("Insufficient balance in your account");
+		}
+
+		public InsufficientBalanceException(String message) {
+			super(message);
+	}
+
+}
