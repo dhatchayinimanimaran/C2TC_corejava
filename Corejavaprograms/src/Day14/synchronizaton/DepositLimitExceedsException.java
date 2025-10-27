79dd9dece69f87977bbdfb6607e1caff085ca78e
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-27 19:41:51 +0530

    commit

---
+++ Corejavaprograms/src/Day14/synchronizaton/DepositLimitExceedsException.java
@@ -0,0 +1,17 @@
+package Day14.synchronizaton;
+
+public class DepositLimitExceedsException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public DepositLimitExceedsException()
+		{
+			super("Daily limit of Deposit is exceeded..");
+		}
+		public DepositLimitExceedsException(String message)
+		{
+			super(message);
+
+	}
+
+}
