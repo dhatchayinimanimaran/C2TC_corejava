1e2ef210ced5c67008d2c83bbd3831b225a45c1c
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-27 19:50:02 +0530

    commit

---
+++ Corejavaprograms/src/Day19/v1/Mycube.java
@@ -0,0 +1,11 @@
+package Day19.v1;
+
+public class Mycube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int getCube(int no);
+		//void show(); not allow more than one abstract method
+	}
+
+}
