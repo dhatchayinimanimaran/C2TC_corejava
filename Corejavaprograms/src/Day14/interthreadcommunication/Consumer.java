b75e4d636f3e6b5b6706dfa1664c0869a9ee77e8
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-27 19:33:49 +0530

    commit

--- Corejavaprograms/src/Day14/interthreadcommunication/Consumer.java
+++ Corejavaprograms/src/Day14/interthreadcommunication/Consumer.java
@@ -4,6 +4,22 @@ public class Consumer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Q obj; 
+
+		public Consumer(Q obj) {
+			this.obj = obj;
+			start();
+		}
+
+		public void run() {
+				while (true) {
+				try {
+					Thread.sleep(3000);
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				obj.get();
+			}
 
 	}
 
