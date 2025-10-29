9236290b34f986106bb93f4db266d353093b0f7a
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-29 12:56:27 +0530

    commit

---
+++ Corejavaprograms/src/Day15/generics/GenericConstructorsDemo.java
@@ -0,0 +1,24 @@
+package Day15.generics;
+
+public class GenericConstructorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				  System.out.println("Number to Double Conversion:");
+				  
+				  GenericConstructors obj=new GenericConstructors(10);
+				  obj.show();
+				  
+				  GenericConstructors objOne=new GenericConstructors(136.8F);
+				  objOne.show();
+				  
+					/*
+					 * GenericConstructors objTwo=new GenericConstructors("Hello")); objTwo.show();
+					 */
+
+			
+
+	}
+
+}
