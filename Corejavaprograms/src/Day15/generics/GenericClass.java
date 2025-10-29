013e16bf8fc1e81f6a416761de0c59d11a1b045e
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-29 12:52:19 +0530

    commit

---
+++ Corejavaprograms/src/Day15/generics/GenericClass.java
@@ -0,0 +1,12 @@
+package Day15.generics;
+
+public class GenericClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class GenericClass {
+
+		}
+	}
+
+}
