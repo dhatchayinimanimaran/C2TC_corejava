c8c0de1750f832885fa8a9b19baa9a2a535867bd
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-30 10:56:33 +0530

    commit

---
+++ Corejavaprograms/src/PredefinedAnnotationDemo/PredefinedAnnotationDemo.java
@@ -0,0 +1,18 @@
+package PredefinedAnnotationDemo;
+
+public class PredefinedAnnotationDemo {
+	
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("rawtypes")
+		List al = new ArrayList();
+		Student s2 = new Student(102, "Rashmi", "ECE");
+		al.add(10);
+		al.add(22.22f);
+		al.add(new Student(101, "Gayatri", "CS"));
+		al.add("SKSJIT");
+		al.add(s2);
+
+	}
