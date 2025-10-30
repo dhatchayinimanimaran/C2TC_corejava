3634240f21dfef2ee64d451ab9276a8c0f46bb3d
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-30 11:01:29 +0530

    commit

---
+++ Corejavaprograms/src/Day21/userdefinedannotation/AndroidSeries.java
@@ -0,0 +1,11 @@
+package Day21.userdefinedannotation;
+
+class AndroidSeries {
+	String model;
+	int screenSize;
+
+	public AndroidSeries(String model, int screenSize) {
+		this.model = model;
+		this.screenSize = screenSize;
+	}
+}
