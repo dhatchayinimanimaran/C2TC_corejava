b0acd20fec67ea84a202a44680d25a2604bf673e
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-30 18:54:52 +0530

    commit

---
+++ Corejavaprograms/src/Day22/v1/employeedb/EmployeeDemo.java
@@ -0,0 +1,24 @@
+package Day22.v1.employeedb;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		try {
+			EmployeeService.showFirst();
+			EmployeeService.showLast();
+			EmployeeService.showPrevious();
+			EmployeeService.showPrevious();
+			EmployeeService.showNext();
+			EmployeeService.updateSalary(65000);
+			
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
